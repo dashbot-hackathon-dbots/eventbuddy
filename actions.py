e9a5d2dbfe633c2bab09f7aad94d8ecb375d9b83
Stéphane Thibaud <snthibaud@gmail.com>
2017-11-05 08:38:39 -0800

    Added images

--- actions.py
+++ actions.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timezone, timedelta
+from random import choice
 
 from microsoftbotframework import ReplyToActivity
+
+from ticketmaster import find_events
 
 
 def _make_dont_understand_reply(message):
@@ -16,6 +19,34 @@ def _date_to_datetime(date):
     return datetime(year=date.year, month=date.month, day=date.day)
 
 
+def _get_event(context):
+    events = find_events(
+        start_date_time=context["start"], end_date_time=context["end"], state_code=context["state_code"],
+        classification_names=context["classification_names"])
+    music_events = [e for e in events for c in e.classifications if c.segment.name == "Music"]
+    event = choice(music_events) if music_events else None
+    if event:
+        images = [i["url"] for i in event.json["images"]]
+        image = images[0] if images else None
+        venues = " / ".join([str(v) for v in event.venues])
+        prices = " / ".join(["{} {} - {}".format(r["currency"], r["min"], r["max"]) for r in event.json.get("priceRanges", [])])
+        classifications = " / ".join(["{} ({})".format(c.genre.name, c.subgenre.name) for c in event.classifications])
+        event = {"name": event.name, "link": event.json["url"], "image": image,
+                 "datetime": "{} {}".format(event.local_start_date, event.local_start_time),
+                 "venues": venues, "classifications": classifications, "prices": prices}
+    return event
+
+
+def _build_event_message(event_dict, message, first=True):
+    if first:
+        prefix = "What do you think about?"
+    else:
+        prefix = "Then what about?"
+    return ReplyToActivity(fill=message, text="{}\n\n{}\n{}\n{}\n{}{}".format(prefix,
+        event_dict["name"], event_dict["classifications"], event_dict["datetime"],
+        event_dict["prices"] + "\n" if event_dict.get("prices") else "",
+        event_dict["venues"]), attachments=[{"contentType": "image/jpeg", "contentUrl": event_dict["image"]}])
+
 def reply_event_query(message, context):
     question = context.get("question")
     message_text = message["text"]
@@ -25,7 +56,7 @@ def reply_event_query(message, context):
             today = _date_to_datetime(datetime.now(tz=timezone.utc).date())
             tomorrow = today + timedelta(days=1)
             if message_text == "today":
-                start = datetime.now(tz=timezone.utc).date()
+                start = _date_to_datetime(datetime.now(tz=timezone.utc).date())
                 end = tomorrow
             elif message_text == "tomorrow":
                 start = tomorrow
@@ -48,18 +79,25 @@ def reply_event_query(message, context):
     start, end, state_code, classification_names = context.get("start"), context.get("end"),\
                                                    context.get("state_code"), context.get("classification_names")
     if start and end and state_code and classification_names:
+        event = _get_event(context)
         if context.get("suggestiongiven"):
             if message_text.lower() in ["yes", "great", "sounds good"]:
+                context = {}
                 context["proposepurchase"] = True
                 reply = ReplyToActivity(fill=message,
                                         text="Great! Let's purchase the tickets then!")
             elif message_text.lower() in ["no", "nah", "meh"]:
-                reply = ReplyToActivity(fill=message, text="Then what about...")
+                if event:
+                    reply = _build_event_message(event, message, first=False)
+                else:
+                    return {}, ReplyToActivity(fill=message, text="Sorry. I couldn't find anything to your taste!")
             else:
                 return context, dont_understand_reply
         else:
-            reply = ReplyToActivity(fill=message,
-                                    text=str(context))  # TODO: Replace with suggestions
+            if event:
+                reply = _build_event_message(event, message, first=True)
+            else:
+                return {}, ReplyToActivity(fill=message, text="Sorry. I couldn't find anything to your taste!")
             context["suggestiongiven"] = True
     elif state_code and classification_names:
         reply = ReplyToActivity(fill=message,
--- ticketmaster.py
+++ ticketmaster.py
@@ -1,15 +1,12 @@
 import ticketpy
 
 
-# def find_events(start_date_time, end_date_time, classification_names, latitude, longitude):
-tm_client = ticketpy.ApiClient('4KEuLm3GnFY5e8ROVqrn8aUvwUYGO4AL')
-pages = tm_client.events.find(
-    classification_name='Hip-Hop',
-    state_code='GA',
-    start_date_time='2017-11-04T20:00:00Z',
-    end_date_time='2017-11-10T20:00:00Z'
-)
-
-for page in pages:
-    for event in page:
-        print(event)
+def find_events(start_date_time, end_date_time, classification_names, state_code):
+    tm_client = ticketpy.ApiClient('4KEuLm3GnFY5e8ROVqrn8aUvwUYGO4AL')
+    pages = tm_client.events.find(
+        classification_name=classification_names,
+        state_code=state_code,
+        start_date_time=start_date_time.isoformat(timespec='seconds') + "Z",
+        end_date_time=end_date_time.isoformat(timespec='seconds') + "Z"
+    )
+    return [e for p in pages for e in p]
