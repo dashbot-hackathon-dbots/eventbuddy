507e2a23786ca3082a65cc9cdc7accfa3c0e8c77
Stéphane Thibaud <snthibaud@gmail.com>
2017-11-04 22:32:32 -0700

    Added config.yaml

--- actions.py
+++ actions.py
@@ -2,4 +2,7 @@ from microsoftbotframework import ReplyToActivity
 
 
 def reply_dont_understand(message):
-    ReplyToActivity(fill=message, text="Say what?!")
+    ReplyToActivity(fill=message, text="Say what?!").send()
+
+
+# def reply_event_query(message):
