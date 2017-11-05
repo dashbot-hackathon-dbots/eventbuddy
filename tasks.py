832714d25544e2236edb66a0d53a1e96b0fe7ea8
Stéphane Thibaud P <snthibaudp@Stephanes-MacBook-Pro.local>
2017-11-04 20:04:13 -0700

    Initial commit

---
+++ tasks.py
@@ -0,0 +1,7 @@
+from microsoftbotframework import ReplyToActivity
+
+
+def echo_response(message):
+    if message["type"] == "message":
+        ReplyToActivity(fill=message,
+                        text=message["text"]).send()
